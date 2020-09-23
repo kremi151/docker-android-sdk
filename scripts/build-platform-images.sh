81a442d829f46c76594990b71359bfbb8898a61b
Michel Kremer <kremi151dev@gmail.com>
2020-09-23 11:39:33 +0200

    Fix build arguments

--- scripts/build-platform-images.sh
+++ scripts/build-platform-images.sh
@@ -8,9 +8,9 @@ while read p; do
   BUILD_TOOLS=$(echo "$p" | cut -d, -f 2)
 
   echo "Build SDK image for $ANDROID_PLATFORM"
-  docker build -t kremi151/android-sdk:$ANDROID_PLATFORM --build-arg ANDROID_PLATFORM_VERSION=$ANDROID_VERSION --build-arg BUILD_TOOLS_VERSION=$BUILD_TOOLS sdk-platform
+  docker build -t kremi151/android-sdk:$ANDROID_PLATFORM --build-arg ANDROID_PLATFORM_VERSION=$ANDROID_PLATFORM --build-arg ANDROID_BUILD_TOOLS_VERSION=$BUILD_TOOLS sdk-platform
 
   echo "Build NDK image for $ANDROID_PLATFORM"
-  docker build -t kremi151/android-ndk:$ANDROID_PLATFORM --build-arg ANDROID_PLATFORM_VERSION=$ANDROID_PLATFORM --build-arg BUILD_TOOLS_VERSION=$BUILD_TOOLS ndk-platform
+  docker build -t kremi151/android-ndk:$ANDROID_PLATFORM --build-arg ANDROID_PLATFORM_VERSION=$ANDROID_PLATFORM --build-arg ANDROID_BUILD_TOOLS_VERSION=$BUILD_TOOLS ndk-platform
 
 done <images.csv
