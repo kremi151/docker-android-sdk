1bcfa7beba42c8cd4edf917c3acaf63662fb60f1
Michel Kremer <kremi151dev@gmail.com>
2020-09-23 12:46:30 +0200

    Push Docker images

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "To be implemented"
-exit 1
+echo "Push SDK base image"
+docker push kremi151/android-sdk:base
+
+echo "Push NDK base image"
+docker push kremi151/android-ndk:base
+
+while read p; do
+  if [[ $p != android-* ]]; then
+    continue
+  fi
+
+  ANDROID_PLATFORM=$(echo "$p" | cut -d, -f 1)
+
+  echo "Push SDK $ANDROID_PLATFORM image"
+  docker push kremi151/android-sdk:$ANDROID_PLATFORM
+
+  echo "Push NDK $ANDROID_PLATFORM image"
+  docker push kremi151/android-ndk:$ANDROID_PLATFORM
+  
+done <images.csv
